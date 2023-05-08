9db86cfb01fa1eaba9ab4ee0ee004d7eac01fe93
itika <itika@mejorainfotech.in>
2023-05-08 17:50:02 +0530

    single

--- single.php
+++ single.php
@@ -1,1 +1,97 @@
-hello
+<?php
+/* this is Post detail */
+
+get_header();
+?>
+<div class="body-content">
+        <!-- for banner section -->
+        <div class="banner-section-wrap">
+            <div class="specer"></div>
+        </div>
+        <!-- end banner section -->
+
+        <!-- news details section -->
+        <div class="news-details-section">
+            <div class="container share-wrap">
+                <div class="back-btn">
+                    <a href="news.html">BACK TO NEWS</a>
+                </div>
+                <div class="news-details-wrap">
+                    <div class="news-img">
+                        <img src="images/news/news-details.jpg" alt="news-details">
+                    </div>
+                    <div class="news-name">
+                        <strong class="date">23 Nov 2021</strong>
+                        <h1>Multinational Enterprises and Tax Administrations: <br>CYPRUS Intends to Espouse OECD Transfer Pricing Guidelines </h1>
+                    </div>
+                    <div class="news-content-text">
+                        <p>Cyprus is mulling the enabling of transfer pricing regulations in an environment rife with base erosion and proﬁt 
+                            shifting. Read on to ﬁnd out about the proposed Transfer Pricing legislation of 2021.  
+                        </p>
+                        <h2>H2 Title</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet 
+                            dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper 
+                            suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in 
+                            vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et 
+                            iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi:
+                        </p>
+                        <ul class="content-list-text">
+                            <li>Duis autem vel eum iriure dolor in </li>
+                            <li>Duis autem vel eum iriure dolor in </li>
+                        </ul>
+                        <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet 
+                            dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper 
+                            suscipit lobortis nisl ut aliquip ex ea commodo consequat.<br>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet 
+                            dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper 
+                            suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in 
+                            vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et 
+                            iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+                        </p>
+                        <h3>H3 Title</h3>
+                        <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet 
+                            dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper 
+                            suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet 
+                            dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper 
+                            suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in 
+                            vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et 
+                            iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet 
+                            dolore magna aliquam erat volutpat. Ut wisi umsan et iusto odio dignissim qui blandit prae
+                        </p>
+                    </div>
+                    <div id="share-box">
+                        <button id="share-btn">
+                          <img src="images/icons/share.svg" alt="share-icon">
+                        </button>
+                      
+                        <ul id="share-list">
+                          <li class="list-item">
+                            <a class="list-item-link" href="#">
+                                <img src="images/icons/facebook.svg" alt="fb">
+                            </a>
+                          </li>
+                          <li class="list-item">
+                            <a class="list-item-link" href="#">
+                                <img src="images/icons/twitter.svg" alt="fb">
+                            </a>
+                          </li>
+                          <li class="list-item">
+                            <a class="list-item-link" href="#">
+                                <img src="images/icons/linkedin.svg" alt="fb">
+                            </a>
+                          </li>
+                        </ul>
+                        <div class="share-text">Share this article</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end details section -->
+    </div>
+    <?php get_footer(); ?>
