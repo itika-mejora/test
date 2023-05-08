c1f309154a52a706223c1d6632efbdcaad8cd97d
itika <itika@mejorainfotech.in>
2023-05-08 19:17:06 +0530

    fn

--- functions.php
+++ functions.php
@@ -29,6 +29,8 @@ function add_theme_scripts() {
     else {
         
         wp_enqueue_style( 'contact-style', get_template_directory_uri() . '/css/contact.min.css',false, null,'all');
+        
+        wp_enqueue_style( 'contact-style', get_template_directory_uri() . 'css/contact.css',false, null,'all');
         wp_enqueue_style( 'country-style', get_template_directory_uri() . '/css/country-select.min.css',false, null,'all');
         
         
