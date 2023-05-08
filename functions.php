c5a99aa36421d949ec453c9c32c2ad69d58ac913
itika <itika@mejorainfotech.in>
2023-05-08 18:46:05 +0530

    fun

--- functions.php
+++ functions.php
@@ -15,14 +15,15 @@ function add_theme_scripts() {
          wp_enqueue_style( 'citizen-style', get_template_directory_uri() . '/css/main.css',false, null,'all');
          wp_enqueue_style( 'citizen-style', get_template_directory_uri() . '/css/style.css',false, null,'all');
 
-    
+//     }
+//      elseif(is_page_template("page-templates/template-blog.php") || is_singular('post')){
+//         wp_enqueue_style( 'news-style', get_template_directory_uri() . '/css/main.css',false, null,'all');
+//         wp_enqueue_style( 'news-body-style', get_template_directory_uri() . '/css/news.min.css',false, null,'all');
 
-     }
-     elseif(is_page_template("page-templates/template-blog.php") || is_singular('post')){
-        wp_enqueue_style( 'news-style', get_template_directory_uri() . '/css/main.css',false, null,'all');
-        wp_enqueue_style( 'news-body-style', get_template_directory_uri() . '/css/news.min.css',false, null,'all');
-
-   
+//    }
+   elseif(is_home() || is_singular('post')){
+    wp_enqueue_style( 'news-style', get_template_directory_uri() . '/css/main.css',false, null,'all');
+    wp_enqueue_style( 'news-body-style', get_template_directory_uri() . '/css/news.min.css',false, null,'all');
 
     }
     else {
