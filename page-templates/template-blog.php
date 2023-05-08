8d7241f220157f893253c0d7f21260c4f51e17fd
itika <itika@mejorainfotech.in>
2023-05-08 17:45:24 +0530

    news

--- page-templates/template-blog.php
+++ page-templates/template-blog.php
@@ -51,12 +51,14 @@ get_header();
                 <?php endif; ?>
                 <?php  wp_reset_query(); ?>
 
-                <?php if($wp_query->max_num_pages > 1 ): ?>
+               
                 <div class="load-more-btn">
-                <a href="javascript:void(0);" ><?php _e( 'LOAD MORE', 'text_domain' ); ?></a>
+                <a href="#!">
+                        LOAD MORE
+                    </a>
                     
                 </div>
-                <?php endif;?>
+                
             </div>
         </duv>
         <!-- end news section -->
