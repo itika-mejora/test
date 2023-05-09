04d5b1669a7b72a3d52d2dcd8c2cff1554ab2cf6
itika <itika@mejorainfotech.in>
2023-05-09 10:27:35 +0530

    fun

--- functions.php
+++ functions.php
@@ -27,7 +27,7 @@ function add_theme_scripts() {
 
     }
     elseif(is_page_template("page-templates/template-contact.php")){
-        wp_enqueue_style( 'contact-style', get_template_directory_uri() . '/css/main.css',false, null,'all');
+       
         wp_enqueue_style( 'contact-style', get_template_directory_uri() . '/css/contact.min.css',false, null,'all');
     
         }
